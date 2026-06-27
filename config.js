3af0114093851ef49b849e4f886fa61270d52fe9
MxM4xCode <kentashi@wp.pl>
2026-06-27 13:32:00 +0200

    Update database connection string and session key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: ' ',
-    keySession: ['TWOJKLUCZ'],
+    db: 'mongodb+srv://Admin:FP589u115YERSc4s@cluster0.zkuyvfr.mongodb.net/?appName=Cluster0',
+    keySession: ['8f9s8df7s9df7s9df7s9df7s9df7s9df'],
     maxAgeSession: 24 * 60 * 60 * 1000 // 24 hours
 }
